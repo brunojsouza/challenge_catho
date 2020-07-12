78061e47f6a21114a4e0a9930c1d14658936da73
Bruno Souza <souza.brunoj@gmail.com>
2020-07-12 15:43:09 -0300

    Fix class properties name.

--- domain/src/main/java/br/com/souzabrunoj/domain/login/LoginModel.kt
+++ domain/src/main/java/br/com/souzabrunoj/domain/login/LoginModel.kt
@@ -1,9 +1,8 @@
 package br.com.souzabrunoj.domain.login
 
 data class LoginModel(
-    val action: String,
-    val createdAt: String,
     val id: String,
-    val message: String,
-    val tipId: String
+    val name: String,
+    val token: String,
+    val photoUrl: String
 )
