99a0a22891fb6eec0d889b60a8f9c01b0d91e1fa
Bruno Souza <souza.brunoj@gmail.com>
2020-07-12 15:44:36 -0300

    Fix widget_card_tips layout.

--- app/src/main/java/br/com/souzabrunoj/challenge_catho/widgets/CardTipsWidget.kt
+++ app/src/main/java/br/com/souzabrunoj/challenge_catho/widgets/CardTipsWidget.kt
@@ -2,6 +2,7 @@ package br.com.souzabrunoj.challenge_catho.widgets
 
 import android.content.Context
 import android.util.AttributeSet
+import androidx.annotation.ColorRes
 import androidx.annotation.DrawableRes
 import androidx.cardview.widget.CardView
 import androidx.core.content.ContextCompat
@@ -14,7 +15,7 @@ class CardTipsWidget @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
 ) : CardView(context, attrs, defStyleAttr) {
 
-    var checkCvClick: (onClick)? = null
+    var buttonClick: (onClick)? = null
     var likeClick: (onClick)? = null
     var unLikeClick: (onClick)? = null
 
@@ -26,6 +27,8 @@ class CardTipsWidget @JvmOverloads constructor(
             textButton = typeArray.getString(R.styleable.CardTipsWidget_button_text)
             likeIcon = typeArray.getResourceId(R.styleable.CardTipsWidget_like_icon, R.drawable.ic_hide_password)
             unlikeIcon = typeArray.getResourceId(R.styleable.CardTipsWidget_unlike_icon, R.drawable.ic_hide_password)
+            buttonTextColor = typeArray.getColor(R.styleable.CardTipsWidget_button_text_color, ContextCompat.getColor(context, R.color.white))
+            buttonBackground = typeArray.getResourceId(R.styleable.CardTipsWidget_button_background, R.drawable.bg_white_background)
 
             typeArray.recycle()
         }
@@ -33,13 +36,13 @@ class CardTipsWidget @JvmOverloads constructor(
         setListeners()
     }
 
-    private var text: String? = null
+    var text: String? = null
         set(value) {
             value?.let { tv_title.text = it }
             field = value
         }
 
-    private var textButton: String? = null
+    var textButton: String? = null
         set(value) {
             value?.let { bt_check_cv.text = it }
             field = value
@@ -59,8 +62,22 @@ class CardTipsWidget @JvmOverloads constructor(
             field = value
         }
 
+    @ColorRes
+    private var buttonTextColor: Int? = null
+    set(value) {
+        value?.let { bt_check_cv.setTextColor(it) }
+        field = value
+    }
+
+    @DrawableRes
+    private var buttonBackground: Int? = null
+        set(value) {
+            value?.let { bt_check_cv.background = ContextCompat.getDrawable(context, it)}
+            field = value
+        }
+
     private fun setListeners() {
-        bt_check_cv.setOnClickListener { checkCvClick?.invoke() }
+        bt_check_cv.setOnClickListener { buttonClick?.invoke() }
         iv_like.setOnClickListener { likeClick?.invoke() }
         iv_unlike.setOnClickListener { unLikeClick?.invoke() }
     }
