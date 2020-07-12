3531ce762e84cf18b22f9e437aa1cecb212fdb6d
Bruno Souza <souza.brunoj@gmail.com>
2020-07-12 16:02:35 -0300

    Add coil library in project and created extension for it.

--- app/src/main/java/br/com/souzabrunoj/challenge_catho/common/ViewExtension.kt
+++ app/src/main/java/br/com/souzabrunoj/challenge_catho/common/ViewExtension.kt
@@ -1,7 +1,18 @@
 package br.com.souzabrunoj.challenge_catho.common
 
 import android.view.View
+import android.widget.ImageView
+import coil.api.load
+import coil.transform.CircleCropTransformation
 
 fun View.invisible() {
     this.visibility = View.INVISIBLE
+}
+
+fun ImageView.loadImage(url: String, placeholder: Int = 0) {
+    this.load(url) {
+        placeholder(placeholder)
+        transformations(CircleCropTransformation())
+        error(placeholder)
+    }
 }
