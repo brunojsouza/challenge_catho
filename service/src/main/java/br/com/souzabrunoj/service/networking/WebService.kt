22c3d97126b06e25601b4f7cab15ac18e995e6e9
Bruno Souza <souza.brunoj@gmail.com>
2020-07-13 18:23:56 -0300

    Added retrofit annotation in api calls methods.

--- service/src/main/java/br/com/souzabrunoj/service/networking/WebService.kt
+++ service/src/main/java/br/com/souzabrunoj/service/networking/WebService.kt
@@ -3,11 +3,15 @@ package br.com.souzabrunoj.service.networking
 import br.com.souzabrunoj.service.networking.data.remote.LoginResponse
 import br.com.souzabrunoj.service.networking.data.remote.PositionResponse
 import br.com.souzabrunoj.service.networking.data.remote.TipResponse
+import retrofit2.http.GET
 
 interface WebService {
+    @GET("keys")
     suspend fun doLogin(): LoginResponse
 
+    @GET("suggestion")
     suspend fun getPositions(): List<PositionResponse>
 
+    @GET("tips")
     suspend fun getTips(): List<TipResponse>
 }
