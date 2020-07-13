04d7ed712f00d39c8bb5bf0eece17f2aa8cb5982
Bruno Souza <souza.brunoj@gmail.com>
2020-07-13 11:03:46 -0300

    Created class ViewState.kt to handle all state in api call.

---
+++ app/src/main/java/br/com/souzabrunoj/challenge_catho/common/ViewState.kt
@@ -0,0 +1,59 @@
+package br.com.souzabrunoj.challenge_catho.common
+
+import androidx.lifecycle.MutableLiveData
+import br.com.souzabrunoj.domain.common.Failure
+
+class ViewState<D>(private val status: ViewStatus, val data: D? = null, val error: Throwable? = null, val failure: Failure? = null) {
+
+    fun handleIt(
+        success: (D) -> Unit = {},
+        error: (Throwable) -> Unit = {},
+        failure: (Failure) -> Unit = {},
+        loading: () -> Unit = {},
+        stopLoading: () -> Unit = {},
+        neutral: () -> Unit = {}
+    ): ViewState<D> {
+
+        when (this.status) {
+            ViewStatus.SUCCESS -> {
+                this.data?.let { success(it) }
+                stopLoading()
+            }
+            ViewStatus.ERROR -> {
+                this.error?.let { error(it) }
+                stopLoading()
+            }
+            ViewStatus.LOADING -> {
+                loading()
+            }
+            ViewStatus.NEUTRAL -> {
+                neutral()
+                stopLoading()
+            }
+            ViewStatus.FAILURE -> {
+                this.failure?.let { failure(it) }
+                stopLoading()
+            }
+        }
+
+        return this
+    }
+
+    enum class ViewStatus {
+        LOADING, SUCCESS, ERROR, NEUTRAL, FAILURE
+    }
+}
+
+fun <T> MutableLiveData<ViewState<T>>.postSuccess(data: T?) {
+    this.postValue(ViewState(ViewState.ViewStatus.SUCCESS, data, null))
+}
+
+fun <T> MutableLiveData<ViewState<T>>.postLoading() {
+    this.postValue(ViewState(ViewState.ViewStatus.LOADING, this.value?.data, this.value?.error))
+}
+
+fun <T> MutableLiveData<ViewState<T>>.postFailure(failure: Failure) {
+    this.postValue(ViewState(ViewState.ViewStatus.FAILURE, failure = failure))
+}
+
+
