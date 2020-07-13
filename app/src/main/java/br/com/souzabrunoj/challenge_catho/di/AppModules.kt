fd445c0484f244b308fc5de2aef712b677f5c79a
Bruno Souza <souza.brunoj@gmail.com>
2020-07-12 23:31:33 -0300

    Added parameter to dependency injection in app module.

--- app/src/main/java/br/com/souzabrunoj/challenge_catho/di/AppModules.kt
+++ app/src/main/java/br/com/souzabrunoj/challenge_catho/di/AppModules.kt
@@ -5,5 +5,5 @@ import org.koin.android.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val presentationModules = module{
-    viewModel { HomeViewModel() }
+    viewModel { HomeViewModel(get()) }
 }
