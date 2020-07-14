628e3d2cffae792c03c7d8d277a153f2fdb31f6a
Bruno Souza <souza.brunoj@gmail.com>
2020-07-14 16:56:42 -0300

    Fix error when load user photo.

--- repository/src/main/java/br/com/souzabrunoj/repository/mappers/LoginMapper.kt
+++ repository/src/main/java/br/com/souzabrunoj/repository/mappers/LoginMapper.kt
@@ -1,11 +1,12 @@
 package br.com.souzabrunoj.repository.mappers
 
 import br.com.souzabrunoj.domain.data.response.login.LoginModel
+import br.com.souzabrunoj.service.BuildConfig
 import br.com.souzabrunoj.service.networking.data.remote.LoginResponse
 
 fun LoginResponse.toLoginModel() = LoginModel(
     id = this.id ?: "",
     name = this.name ?: "",
     token = this.token ?: "",
-    photoUrl = this.photoUrl ?: ""
+    photoUrl = "${BuildConfig.BASE_URL}${this.photoUrl?.drop(1)}"
 )
