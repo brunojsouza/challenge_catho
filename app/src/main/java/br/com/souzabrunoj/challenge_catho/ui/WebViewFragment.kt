d8b2b4eee90252d58f9324015508504b2485b83e
Bruno Souza <souza.brunoj@gmail.com>
2020-07-13 23:20:43 -0300

    Create WebViewFragment.kt.

---
+++ app/src/main/java/br/com/souzabrunoj/challenge_catho/ui/WebViewFragment.kt
@@ -0,0 +1,38 @@
+package br.com.souzabrunoj.challenge_catho.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.webkit.WebResourceRequest
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.navigation.fragment.navArgs
+import br.com.souzabrunoj.challenge_catho.R
+import br.com.souzabrunoj.challenge_catho.ui.base.BaseFragment
+import kotlinx.android.synthetic.main.fragment_web_view.*
+
+class WebViewFragment : BaseFragment() {
+
+    private val args: WebViewFragmentArgs  by navArgs()
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_web_view, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupWebView()
+        //setupToolbar(args.bookName)
+    }
+
+    private fun setupWebView(){
+        wv_container.loadUrl(args.url)
+        wv_container.webViewClient = object : WebViewClient() {
+            override fun shouldOverrideUrlLoading(view: WebView?, request: WebResourceRequest?): Boolean {
+                view?.loadUrl(request?.url.toString())
+                return true
+            }
+        }
+    }
+}
