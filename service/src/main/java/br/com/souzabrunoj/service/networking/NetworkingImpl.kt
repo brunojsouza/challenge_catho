ef135421ddf2cfaa6cc45adb7dba0544977fea39
Bruno Souza <souza.brunoj@gmail.com>
2020-07-14 12:01:03 -0300

    Added config build variable to control enable e disable mock call.

--- service/src/main/java/br/com/souzabrunoj/service/networking/NetworkingImpl.kt
+++ service/src/main/java/br/com/souzabrunoj/service/networking/NetworkingImpl.kt
@@ -1,7 +1,9 @@
 package br.com.souzabrunoj.service.networking
 
+import br.com.souzabrunoj.service.BuildConfig
 import br.com.souzabrunoj.service.common.BaseNetworking
 import br.com.souzabrunoj.service.networking.data.key.KeyResponse
+import br.com.souzabrunoj.service.networking.data.mock.*
 import br.com.souzabrunoj.service.networking.data.remote.LoginResponse
 import br.com.souzabrunoj.service.networking.data.remote.PositionResponse
 import br.com.souzabrunoj.service.networking.data.remote.SurveyResponse
@@ -9,16 +11,18 @@ import br.com.souzabrunoj.service.networking.data.remote.TipResponse
 
 class NetworkingImpl(private val webService: WebService) : Networking, BaseNetworking() {
 
-    override suspend fun getApiKey(): KeyResponse = doRequest { webService.getApiKey() }
+    override suspend fun getApiKey(): KeyResponse = if (BuildConfig.ENABLE_MOCK) doApiKeysFromMock() else doRequest { webService.getApiKey() }
 
-    override suspend fun doLogin(apiKey: String, userId: String): LoginResponse = /*doLoginFromMock()*/
+    override suspend fun doLogin(apiKey: String, userId: String): LoginResponse = if (BuildConfig.ENABLE_MOCK) doLoginFromMock() else
         doRequest { webService.doLogin(apiKey, userId) }
 
-    override suspend fun getPositions(apiKey: String): List<PositionResponse> =  /*getPositionsFromMock()*/
+    override suspend fun getPositions(apiKey: String): List<PositionResponse> = if (BuildConfig.ENABLE_MOCK) getPositionsFromMock() else
         doRequest { webService.getPositions(apiKey) }
 
-    override suspend fun getTips(apiKey: String): List<TipResponse> = /*getTipsFromMock()*/ doRequest { webService.getTips(apiKey) }
+    override suspend fun getTips(apiKey: String): List<TipResponse> =
+        if (BuildConfig.ENABLE_MOCK) getTipsFromMock() else doRequest { webService.getTips(apiKey) }
 
-    override suspend fun sendTipSurvey(apiKey: String, tipId: String, interactionType: String): SurveyResponse = /*sendTipSurveyFromMock()*/
-        doRequest { webService.sendTipSurvey(apiKey, tipId, interactionType) }
+    override suspend fun sendTipSurvey(apiKey: String, tipId: String, interactionType: String): SurveyResponse =
+        if (BuildConfig.ENABLE_MOCK) sendTipSurveyFromMock() else
+            doRequest { webService.sendTipSurvey(apiKey, tipId, interactionType) }
 }
