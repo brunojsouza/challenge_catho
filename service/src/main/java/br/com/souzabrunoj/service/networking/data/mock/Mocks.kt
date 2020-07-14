facdd515221f8df698d625b2c5f5dd73034788b0
Bruno Souza <souza.brunoj@gmail.com>
2020-07-14 12:00:12 -0300

    Create mock to get api key call method.

--- service/src/main/java/br/com/souzabrunoj/service/networking/data/mock/Mocks.kt
+++ service/src/main/java/br/com/souzabrunoj/service/networking/data/mock/Mocks.kt
@@ -1,9 +1,12 @@
 package br.com.souzabrunoj.service.networking.data.mock
 
+import br.com.souzabrunoj.service.networking.data.key.KeyResponse
 import br.com.souzabrunoj.service.networking.data.remote.LoginResponse
 import br.com.souzabrunoj.service.networking.data.remote.PositionResponse
 import br.com.souzabrunoj.service.networking.data.remote.SurveyResponse
 import br.com.souzabrunoj.service.networking.data.remote.TipResponse
+
+fun doApiKeysFromMock(): KeyResponse = fromJson("keys_response.json")
 
 fun doLoginFromMock(): LoginResponse = fromJson("login_response.json")
 
